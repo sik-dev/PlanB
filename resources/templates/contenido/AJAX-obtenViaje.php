3aae52dbadbd34fed6decaa98a28950b6f358d3b
JorgeLacasta <jorge_lacasta@hotmail.com>
2020-04-15 12:26:19 +0200

    correccion de error en Ajax de obtenViaje.php

--- resources/templates/contenido/AJAX-obtenViaje.php
+++ resources/templates/contenido/AJAX-obtenViaje.php
@@ -4,12 +4,17 @@ error_reporting(0);
 
 if(isset($_GET['id']) && !empty($_GET['id'])) {
     $id = $_GET['id'];
-    $datos = ViajesManager::getViajesID($id)[0];
-    $datosItinerario = ViajesManager::getItinerario($id);
+    $datos = ViajeManager::getById($id);
+    $datosItinerario = ItinerarioManager::getBy($id);
     //print_r(json_encode([$datos, $datosItinerario]));
     //$obj = json_encode([$datos, $datosItinerario));
 
-
+    /*
+    echo "<pre>";
+    print_r($datos);
+    print_r($datosItinerario);
+    echo "</pre>";
+    */
     for ($i=0; $i < count($datosItinerario); $i++) {
       $datosItinerario[$i]['foto'] = FotosItinerarioManager::getBy($datosItinerario[$i]['id'])[0]['ruta'];
     }
