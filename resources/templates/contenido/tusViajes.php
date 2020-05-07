6a1e65536e2d234d07a25a92d34cf289cf801d79
JorgeLacasta <jorge_lacasta@hotmail.com>
2020-05-07 17:51:29 +0200

    css tus viajes

--- resources/templates/contenido/tusViajes.php
+++ resources/templates/contenido/tusViajes.php
@@ -3,38 +3,79 @@
     header('Location: login.php');
     exit;
   }
+  $contador = 0;
+  $etiquetas = [];
+
   $datos = ViajeManager::getBy($_SESSION['id']);
+
+
+  for ($i = 0; $i < count($datos); $i++) {
+    $etiquetas[$i] = explode('/', $datos[$i]['viaje']->getEtiquetas());
+  }
+
 
 ?>
 <link rel="stylesheet" href="/css/tusViajes.css">
-<div class="inicio">
-   <h1>Tus viajes</h1>
-   <?php if( count($datos) > 0) { ?>
-   <div class="mejorValorados">
-     <?php foreach ($datos as $fila) { ?>
-       <div class="viaje">
-         <h2><?=$fila['viaje']->getCiudadDestino()?></h2>
-         <h4><?=$fila['viaje']->getDescripcion()?></h4>
-         <a href="viaje.php?id=<?=$fila['viaje']->getId()?>">
-          <img id='mejoresValorados' src="imgs/<?=$fila['viaje']->getIdUser()."/".$fila['viaje']->getFoto()?>" alt="">
-         </a>
-         <div class="datos">
-           <p>Precio: <?=$fila['viaje']->getPrecio()?></p>
-           <p>Transporte: <?=$fila['viaje']->getTransporte()?></p>
-           <a href="borrarViaje.php?id_viaje=<?=$fila['viaje']->getId()?>&id_user=<?=$_SESSION['id']?>">
-             <img src="/logos_proyecto/basura.png" alt="Borrar viaje" class="borrarViaje">
-           </a>
-         </div>
-       </div>
-     <?php } ?>
-   </div>
- <?php }else{ ?>
-   <div class="tusViajes">
+
+<div class="tusViajes">
+
+  <div class="imagen">
+    <h1>Todos los destinos al alcance de tu mano</h1>
+  </div>
+
+  <?php if($datos == null) { ?>
+    <div class="noViajes">
      <h2>Aún no has subido ningún viaje</h2>
      <br>
-     <p>¿Quieres subir un viaje?</p>
-     <a href="crearViaje_1.php?id=<?=$_SESSION['id']?>">Subir viaje</a>
-   </div>
- <?php } ?>
+     <div>
+      <p>¿Quieres subir un viaje?</p>
+      <a href="crearViaje_1.php?id=<?=$_SESSION['id']?>">Subir viaje</a>
+     </div>
 
+    </div>
+  <?php }else{ ?>
+    <h2>Tus Viajes:</h2>
+
+    <div class="contenedor">
+
+      <?php foreach ($datos as $fila) { ?>
+        <div class='tarjeta'>
+          <a href="viaje.php?id=<?=$fila['viaje']->getId()?>">
+            <div class="puntuacion">
+              <?php for($i=1; $i <= 5; $i++) { ?>
+                <?php if ($fila['media'] >= $i) { ?>
+                  <span class='rellena'>☆</span>
+                <?php }else{ ?>
+                  <span>☆</span>
+                <?php } ?>
+              <?php } ?>
+              <span><?=$fila['media']?></span>
+            </div>
+          
+            <img src="imgs/<?=$fila['viaje']->getIdUser()."/".$fila['viaje']->getFoto()?>" alt="">
+          
+            <h3><?=$fila['viaje']->getCiudadDestino()?></h3>
+            <div>
+              <div class="datos">
+                  <p><?=($fila['diasViaje'] > 1)?  $fila['diasViaje']." días" : $fila['diasViaje']." día" ?></p>
+                  <p><?=$fila['viaje']->getPrecio()?> &euro;</p>
+              </div>
+              <div class="etiquetas">
+                <?php foreach ($etiquetas[$contador] as $valor) {?>
+                  <span class="<?=($valor =='Con amig@s')?'Amigos':$valor ?>" title="<?=$valor?>" alt='<?=$valor?>'></span>
+                <?php } ?>
+              </div>
+              <?php $contador++ ?>
+            </div>
+          </a>
+          <a href="borrarViaje.php?id_viaje=<?=$fila['viaje']->getId()?>&id_user=<?=$_SESSION['id']?>">
+             <img id='basura' src="/logos_proyecto/basura.png" alt="Borrar viaje" class="borrarViaje">
+            </a>
+        </div>
+      <?php } ?>
+
+    </div>
+
+  <?php }?>
+  
 </div>
