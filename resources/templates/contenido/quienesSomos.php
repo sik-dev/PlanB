c5669271480f55f38f74118ac99eeeaffdd04c50
JorgeLacasta <jorge_lacasta@hotmail.com>
2020-05-11 16:46:21 +0200

    css footer y test

--- resources/templates/contenido/quienesSomos.php
+++ resources/templates/contenido/quienesSomos.php
@@ -3,16 +3,31 @@
 ?>
 <link rel="stylesheet" href="/css/quienesSomos.css">
 <div class="quienesSomos">
-  <h1>Plan B</h1>
-  <br>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-     aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-     velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-      sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
-  <br>
-  <p>Proyecto creado por Jorge, Adrián y Joan</p>
+
+  <div class="imagen">
+    <h1>Todos los destinos al alcance de tu mano</h1>
+  </div>
+
+  <h2>Plan B</h2>
+
+  <div class='texto'>
+    <p>
+      Esta empresa ha sido creada por los alumnos Jorge Lacasta y Joan Crisóstomo. Nuestra empresa, llamada 'PLAN B', es una red social de viajes. En nuestra web los usuarios podrán compartir sus viajes favoritos y ver los viajes que otros usuarios han subido a la web. De esta forma, le gente podrá recalar muchas ideas para futuros viajes, y comparar las distintas opciones que ofrece un mismo destino. Podremos obtener comentarios y puntuaciones de otros usuarios, y a su vez, recibir nosotros en nuestro viaje estas puntuaciones y comentarios.
+    </p>
+    <p>
+    Nuestra empresa se basa en las experiencias y vivencias de la gente que ha experimentado en sus viajes. De este modo queremos convertirnos en un amplio portal de viajes, donde los usuarios puedan programar sus futuros viajes de una manera más fácil y barata. No queremos ofrecer servicios de comparación de vuelos u hoteles como otras webs, nuestra función consiste en ofrecer al usuario varias opciones de un mismo destino y aprovecharse de consejos de las personas que han realizado el viaje. También agilizará la preparación del itinerario a seguir en los viajes, ya que podremos visitar los mismos sitios de la persona que subió el viaje y realizar su itinerario y visitar los mismos sitios.
+    </p>
+    <p>
+    Actualmente, los viajes por todo el mundo se han incrementado a lo largo de los años, y seguirán creciendo con el paso del tiempo. Gracias a aerolíneas más baratas ha sido posible que mucha más gente viaje a otros países y se haya formado unas costumbres nuevas que incitan a viajar y descubrir otros lugares. Esto, unido a la tecnología y las redes sociales hacen que todo el mundo quiera compartir sus experiencias con los demás. Nuestra web atraerá a mucha gente que este acostumbrada a viajar y quiera compartir sus viajes y rutinas con otras personas. Puede ganar fama y puntuación dentro de la aplicación. Las nuevas tendencias de los influencers encajarán a la perfección con nuestra web porque son gente que realiza muchos viajes y ayuda a sus seguidores a decidir y a aconsejar. La web también esta diseñada para un público con no sea tan propenso a viajar y de esta forma pueda usar el itinerario de un viaje de otra persona y realizar la misma experiencia que la persona que ha subido el viaje, sin tener que investigar o recalar información del sitio que va a visitar. Simplemente se descarga el viaje que quiere hacer y se olvida de que sitios visitar o cual es el mejor restaurante para comer en esa ciudad.
+    </p>
+    <p>
+    A día de hoy hay muchas webs y portales que comparan vuelos y hoteles, pero ninguna de ellas registra un viaje completo. Con nuestra web se registrará día a día el itinerario seguido en el viaje y se adjuntará consejos y recomendaciones que puedan servir de utilidad para próximos usuarios que quieran realizar el viaje. De esta forma, los usuarios se podrán ahorrar mucho tiempo en buscar que ver y que es más interesante. También podrán ahorrar dinero, porque no tendrán que recurrir a ninguna empresa de grandes viajes, sino que al organizarse ellos mismo el viaje el precio final puede ser un 50% más barato.
+    </p>
+    <br>
+    <p>Proyecto creado por Jorge Lacasta y Joan Crisóstomo, alumnos de DAW del Instituto Juan de la Cierva.</p>
+    <br>
+    <p>2020</p>
+  </div>
+
 
 </div>
