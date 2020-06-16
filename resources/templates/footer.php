99b29deba43545296404cf868bd0f53cd68f5253
JorgeLacasta <jorge_lacasta@hotmail.com>
2020-06-16 16:03:47 +0200

    arreglo css en el footer

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,15 +1,35 @@
 <link rel="stylesheet" href="/css/footer.css">
 <footer>
-  <div class="">
-    <h4>©2020 clase 2 Daw</h4>
-    <a href="quienesSomos.php">¿Quienes somos?</a>
-    <a href="TestAllViajes.php">TEST</a>
+  <div>
+    <div>
+      <p>PLAN B</p>
+      <p>Red social de viajes</p>
+    </div>
+
+    <div class="enlaces">
+      <p>Menu</p>
+      <a href="inicio.php">Inicio</a>
+      <a href="login.php">Login</a>
+      <a href="quienesSomos.php">¿Quienes somos?</a>
+      <a href="TestAllViajes.php">TEST</a>
+    </div>
+
+    <div class="social">
+      <p>Contacto</p>
+      <p>
+        <span></span>
+        daw.planb@gmail.com
+      </p>
+      <a href="https://www.facebook.com/" target="_blank"></a>
+      <a href="https://twitter.com/" target="_blank"></a>
+      <a href="https://www.youtube.com/" target="_blank"></a>
+      <a href="https://www.instagram.com/" target="_blank"></a>
+    </div>
   </div>
 
-  <div class="social">
-    <a href="https://www.facebook.com/" target="_blank"></a>
-    <a href="https://twitter.com/" target="_blank"></a>
-    <a href="https://www.youtube.com/" target="_blank"></a>
-    <a href="https://www.instagram.com/" target="_blank"></a>
+  <div class='copyright'>
+    <p>Todos los derechos reservados</p>
+    <p>Copyright©2020</p>
   </div>
+
 </footer>
